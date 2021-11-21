585b82041537db3fde5cfd760adf29fcdfa3f012
arif <muhdarifrawi@gmail.com>
2021-11-21 11:48:06 +0800

    Edited exisiting script.js: - changed file structure - changed file name to history.js and created a new script.js file - output data into cards temporarily

---
+++ js/history.js
@@ -0,0 +1,64 @@
+const weather2h = "";
+const weather24h = "";
+const weather4day= "";
+const weatherHistory = "https://data.gov.sg/api/action/datastore_search";
+
+let yearMthValue = "2010-06";
+
+let yearMth;
+
+async function dataSet() {
+    try {
+      const response = await axios.get('https://data.gov.sg/api/action/datastore_search?',
+        {   
+            params: { 
+                resource_id: '1e478275-0746-483d-9783-2f40a3535910', // the resource id
+                fields:"date,station,daily_rainfall_total, mean_temperature, mean_wind_speed"
+            }
+        })
+        let historicalData = response.data.result.records;
+        // console.log(historicalData);
+        
+        filterYearMth(historicalData,yearMthValue);   
+        displayHistoricalData(); 
+        
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+function filterYearMth(data,ymVal){
+    yearMth = data.filter(function(i){
+        if(i.date.slice(0,7)==ymVal){
+            return i
+        }
+        // return i.date
+    })
+    console.log("look here", yearMth);
+    
+}   
+
+dataSet();
+
+function displayHistoricalData(){
+  document.getElementById("content").innerHTML = `<h1>${yearMth.length} results found</h1>`;
+  for(i=0;i<yearMth.length;i++){
+    document.getElementById("content").innerHTML += `
+    <div class="card m-3" style="width: 30rem;">
+      <div class="card-body">
+        
+        <h1>Date</h1>
+        <p>${yearMth[i]["date"]}</p>
+        <h1>total daily rainfall</h1>
+        <p>${yearMth[i]["daily_rainfall_total"]} mm</p>
+        <h1>average temperature</h1>
+        <p>${yearMth[i]["mean_temperature"]} &deg;c</p>
+        <h1>average wind speed</h1>
+        <p>${yearMth[i]["mean_wind_speed"]} km/h</p>
+      </div>
+    </div>
+    
+    `;
+  }
+
+}
