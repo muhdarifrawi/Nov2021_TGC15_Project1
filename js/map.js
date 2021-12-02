ee53a95a1fc66439a1d04f251fc5bfca23a153dc
muhdarifrawi <muhdarifrawi@gmail.com>
2021-12-02 20:03:22 +0800

    Made the following changes: - Added map access token. - Created function to create map. - map sized and shows Singapore map. - Created function for default nav actions.

--- js/map.js
+++ js/map.js
@@ -1,8 +1,23 @@
 window.addEventListener('load', (event) => {
     console.log('map triggered');
-    var map = L.map('map', {
-        center: [51.505, -0.09],
-        zoom: 13
-    });
+    createMap();
     
   });
+
+function createMap(){
+    var map = L.map('map', {
+        center: [1.3521, 103.8198],
+        zoom: 12
+    });
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibXVoZGFyaWZyYXdpIiwiYSI6ImNrd293azJ3MDA2enIyb3BtODE1cWgzZXYifQ.QOBe4u_Sh40mGIx4C8wYPw'
+}).addTo(map);
+
+}
+
--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ let navLink = document.getElementsByClassName("nav-link");
 
 window.addEventListener('load', (event) => {
     console.log('script triggered');
+    defaultActiveNav();
+  });
+
+function defaultActiveNav(){
     document.getElementsByClassName("nav-link")[0].classList.add("active");
     
     for (let i = 0; i < navLink.length; i++) {
         navLink[i].addEventListener('click', toggleActiveNav);
     }
-  });
-
+}
 
 function toggleActiveNav(){
     for (let i = 0; i < navLink.length; i++) {
