b63ad8d0652d7960ea8af0412b5c123538d2b827
arif <muhdarifrawi@gmail.com>
2021-12-02 19:45:17 +0800

    preliminary addition of map script.

---
+++ js/map.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', (event) => {
+    console.log('map triggered');
+    var map = L.map('map', {
+        center: [51.505, -0.09],
+        zoom: 13
+    });
+    
+  });
